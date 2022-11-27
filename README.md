7062eda3c7183050d32ccaf4789e0c6723b2bcde
MuhammadZariyanAsif123 <33691797+MuhammadZariyanAsif123@users.noreply.github.com>
2022-11-27 23:18:02 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# News-Monkey-Application
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-bza8cg)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './style.css';
+import Navbar from './components/Navbar';
+
+export default class App extends Component 
+{
+  render()
+  {
+    return(
+      
+     <div> <Navbar/> </div>
+    )
+  }
+}
+ 
