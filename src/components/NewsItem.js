b653df34a288c5cc0cebdeb64cd2e539262f53b9
MuhammadZariyanAsif123 <33691797+MuhammadZariyanAsif123@users.noreply.github.com>
2022-11-28 13:32:58 +0500

    1 file added

---
+++ src/components/NewsItem.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'React';
+
+export default class NewsItem extends Component
+{
+
+   render()
+   {
+      return(
+          <h5>Hello World</h5>
+      )
+   }
+
+  
+   
+     
+}
