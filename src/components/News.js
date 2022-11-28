20656e8ed7736c9cce92481d6f484c90a70db99a
MuhammadZariyanAsif123 <33691797+MuhammadZariyanAsif123@users.noreply.github.com>
2022-11-28 18:31:12 +0500

    3 files modified, 1 added

--- src/components/News.js
+++ src/components/News.js
@@ -1,13 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { render } from 'react-dom';
 import NewsItem from './NewsItem';
+import { Component } from 'react';
 
-export default class News extends Component 
-{
-  render() 
-  {
+export default class News extends Component {
+  render() {
     return (
-      <div>
-         <NewsItem/>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <NewsItem title="myTitle" description="myDesc" />
+          </div>
+
+          <div class="col-md-4">
+            <NewsItem title="myTitle" description="myDesc" />
+          </div>
+
+          <div class="col-md-4">
+            <NewsItem title="myTitle" description="myDesc" />
+          </div>
+        </div>
+        
+        <div class="row">
+          <div class="col-md-4">
+            <NewsItem title="myTitle" description="myDesc" />
+          </div>
+
+          <div class="col-md-4">
+            <NewsItem title="myTitle" description="myDesc" />
+          </div>
+
+          <div class="col-md-4">
+            <NewsItem title="myTitle" description="myDesc" />
+          </div>
+        </div>
       </div>
     );
   }
